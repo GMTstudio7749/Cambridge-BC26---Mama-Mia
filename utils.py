9b9521d915b799d9896c03c250c51cc2714fde6c
GMTstudio7749 <gmtstudio7749@gmail.com>
2026-03-21 06:57:05 +0700

    Add Diagonal_Dirs to utils.py

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from cambc import Direction, Environment
 
 All_Dirs = [d for d in Direction]
 Dirs = [d for d in Direction if d != Direction.CENTRE]
+Diagonal_Dirs = [Dirs[x] for x in range(1, 8, 2)]
 
 Ore_Env = [Environment.ORE_TITANIUM, Environment.ORE_AXIONITE]
 # CORE
