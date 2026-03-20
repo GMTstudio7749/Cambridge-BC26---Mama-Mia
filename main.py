b6b8a91ee6e992005655684aff405d80dc02ad11
GMTstudio7749 <gmtstudio7749@gmail.com>
2026-03-21 06:56:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from cambc import Controller, EntityType
 from core import Core
 from builder import Builder
-from utils import *
 
 class Player:
 	def __init__(self):
-		self.builder_ctrl = Builder()
 		self.core_ctrl = Core()
+		self.builder_ctrl = Builder()
+		self.setup = False
 
 	def run(self, ct: Controller):
 		my_type = ct.get_entity_type()
 		
-		if(my_type == EntityType.CORE):
+		if my_type == EntityType.CORE:
+			if not self.setup:
+				self.core_ctrl.CORE_setup(ct)
+				self.setup = True
 			self.core_ctrl.CORE_run(ct)
-		elif(my_type == EntityType.BUILDER_BOT):
+		elif my_type == EntityType.BUILDER_BOT:
+			if not self.setup:
+				self.builder_ctrl.BUILDER_setup(ct)
+				self.setup = True
 			self.builder_ctrl.BUILDER_run(ct)
