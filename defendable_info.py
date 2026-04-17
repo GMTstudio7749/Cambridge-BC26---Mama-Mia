b484d3a93f7fa56b6020e081182122f222ecdbb1
GMTstudio7749 <gmtstudio7749@gmail.com>
2026-04-17 21:17:40 +0700

    Delete defendable_info.py

--- defendable_info.py
+++
@@ -1,8 +0,0 @@
-from cambc import Environment, Position
-
-class DefendableInfo:
-    __slots__ = ("pos", "score", "type", "ignore")
-
-    def __init__(self, pos, score):
-        self.pos = pos
-        self.score = score
