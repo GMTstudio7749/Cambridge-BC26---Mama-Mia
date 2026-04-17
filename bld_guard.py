3bcd54005efeca106e3302281c766cc9edb77803
harrySquires123 <theprimeagent123@gmail.com>
2026-04-18 03:29:16 +0700

    Add files via upload

--- bld_guard.py
+++ bld_guard.py
@@ -41,6 +41,9 @@ class BldGuard():
         self.ally_bots = {}
         self.enemy_bots = {}
 
+        self.fix_connects = set()
+        self.fix_target = Position(-1, -1)
+        
     def GUARD_sense_nearby(self, ct):
         self.ally_bots = {}
         self.enemy_bots = {}
@@ -107,6 +110,61 @@ class BldGuard():
             return best_defendable
         return Position(-1, -1)
 
+    def IS_connect_broken(self, ct, pos, bid, btype, bteam):
+        if(bteam != ct.get_team()):
+            return False
+        if(btype != EntityType.CONVEYOR and btype != EntityType.BRIDGE and not (btype == EntityType.HARVESTER and not ctx.CHECK_ore_linked_core(ct, pos))):
+            return False
+        nextPos = Position(-1, -1)
+        if(btype == EntityType.CONVEYOR):
+            conDir = ct.get_direction(bid)
+            nextPos = pos.add(conDir)
+        if(btype == EntityType.BRIDGE):
+            nextPos = ct.get_bridge_target(bid)
+        if(btype == EntityType.HARVESTER):
+            return True
+        if(not ct.is_in_vision(nextPos)):
+            return False
+        bid = ct.get_tile_building_id(nextPos)
+        btype = ct.get_entity_type(bid)
+        bteam = ct.get_team(bid)
+
+        if(bteam != ct.get_team() or (btype != EntityType.CONVEYOR and btype != EntityType.BRIDGE and btype != EntityType.CORE and btype != EntityType.FOUNDRY and btype != EntityType.SPLITTER)):
+            return True
+        return False
+
+    def GET_start_build_pos(self, ct: Controller, ore_pos: Position):
+        """Choose start building pos to LINK BACK CORE"""
+        best_pos = Position(-1, -1)
+        min_dis = 9999
+        dxy = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        prior = "ORE"
+        for dx, dy in dxy:
+            check_pos = Position(ore_pos.x + dx, ore_pos.y + dy)
+            if not ctx.IS_in_map(check_pos): continue
+            if not ct.is_in_vision(check_pos): continue
+
+            # Empty env only
+            env = ct.get_tile_env(check_pos)
+            if env == Environment.WALL: continue
+            if env == Environment.ORE_TITANIUM and prior != "ORE": continue
+
+            # Check for buildings
+            ID = ct.get_tile_building_id(check_pos)
+            if ID is not None:
+                type = ct.get_entity_type(ID)
+                if type == EntityType.HARVESTER: continue
+                if ct.get_team(ID) != ctx.MY_TEAM:
+                    if not type in [EntityType.ROAD, EntityType.MARKER]:
+                        continue
+
+            if env != Environment.ORE_TITANIUM: prior = "EMPTY"
+            # Compare distance
+            dis = check_pos.distance_squared(ctx.CORE_POS)
+            if best_pos == Position(-1, -1) or dis < min_dis:
+                best_pos, min_dis = check_pos, dis
+        return best_pos
+    
     def GET_defendable_info(self, ct, defendable_pos):
         env = ct.get_tile_env(defendable_pos)
         bid = ct.get_tile_building_id(defendable_pos)
@@ -116,6 +174,12 @@ class BldGuard():
         bmaxhp = ct.get_max_hp(bid)
 
         score = 0
+
+        if(self.IS_connect_broken(ct, defendable_pos, bid, btype, bteam)):
+            self.fix_connects.add(defendable_pos)
+        else:
+            if(defendable_pos in self.fix_connects):
+                self.fix_connects.remove(defendable_pos)
 
         if(defendable_pos == self.defendable_target):
             score -= 2
@@ -249,7 +313,7 @@ class BldGuard():
         print(f"MAX accept range: {self.HS_Accept_Range}")
         print(f"MAX visit turn: {self.HS_Visit_Turn}")
         # Draw
-        ct.draw_indicator_dot(self.Spot_Target, 200, 255, 200)
+        # ct.draw_indicator_dot(self.Spot_Target, 200, 255, 200)
     
     def DEBUG_guard_info(self, ct: Controller):
         """Debug function for guardian info"""
@@ -259,7 +323,7 @@ class BldGuard():
         print(f"Core HP: {self.Core_HP}")
         print(f"Target Heal: ({self.Target_Heal_Pos})")
         # Draw
-        ct.draw_indicator_dot(self.Target_Heal_Pos, 100, 255, 100)
+        # ct.draw_indicator_dot(self.Target_Heal_Pos, 100, 255, 100)
     #endregion
 
     #region ----- Guard MOVE / ACT -----
@@ -282,11 +346,25 @@ class BldGuard():
 
     #endregion
 
+    def GET_best_fix_target(self, ct):
+        bestDist = 9999
+        bestPos = Position(-1, -1)
+        for pos in self.fix_connects:
+            if(pos.distance_squared(ct.get_position()) < bestDist):
+                bestPos = pos
+                bestDist = pos.distance_squared(ct.get_position())
+        return bestPos
+
     #region ----- Guard STATE -----
     def GUARD_wandering(self, ct: Controller):
         """Guardian move to hotspot / wandering for work function"""
         self.defendable_target = self.GET_best_defendable_target(ct)
-        if(self.defendable_target == Position(-1, -1)):
+        self.fix_target = self.GET_best_fix_target(ct)
+
+        if(self.fix_target != Position(-1, -1)):
+            self.state = "LINK_BACK_TO_CORE"
+            return
+        elif(self.defendable_target == Position(-1, -1)):
             if ct.get_move_cooldown() == 0:
                 self.GUARD_visit_hotspot(ct)
         else:
@@ -308,11 +386,44 @@ class BldGuard():
             
     #endregion
 
+    def GUARD_link_back_core(self, ct: Controller, start_build_pos: Position):
+        """ECO builder link harvester back to core
+        Depends on start build position"""
+        # DESTROY under foot blocks first
+        my_pos = ct.get_position()
+        if ctx.bugnav.lastConnect != Position(-1, -1):
+            dis_to_connect = my_pos.distance_squared(ctx.bugnav.lastConnect)
+            if dis_to_connect <= 2:
+                ID = ct.get_tile_building_id(my_pos)
+                if ID is not None:
+                    if ct.get_team(ID) != ctx.MY_TEAM:
+                        if ct.can_fire(my_pos):
+                            ct.fire(my_pos)
+                        return "FIRING"
+
+        # BUILD next conv
+        link = ctx.bugnav.MOVE_to_target_with_conveyor(ct, start_build_pos, ctx.CORE_POS)
+        if ctx.bugnav.lastConnect.distance_squared(ctx.CORE_POS) <= 2:
+            ctx.bugnav.lastConnect = Position(-1, -1)
+            return "DONE"
+        if link == "STUCK":
+            ctx.bugnav.lastConnect = Position(-1, -1)
+            return "STUCK"
+    #endregion
+
+    #region ----- ECO state -----
+    def ECO_switch_explore(self, ct: Controller):
+        """Switch state "EXPLORE" immediately in order to save cooldown"""
+        self.state = "EXPLORE"
+        self.fix_target = Position(-1, -1)
+
     def GUARD_run(self, ct: Controller):
         """Main GUARDIAN builder function"""
         if not self.setup:
             self.setup = True
             self.GUARD_setup(ct)
+
+        print(self.state)
 
         # UPDATE
         ctx.ORE_update(ct)
@@ -320,9 +431,26 @@ class BldGuard():
         print("NOW1: ", ct.get_cpu_time_elapsed())
 
         self.GUARD_update_info(ct)
+        if(self.state == "EXPLORE"):
+            self.GUARD_wandering(ct)
+        elif(self.state == "LINK_BACK_TO_CORE"):
+            ct.draw_indicator_line(ct.get_position(),  self.fix_target, 255, 255, 0)
+            if(ct.is_in_vision(self.fix_target)):
+                bid = ct.get_tile_building_id(self.fix_target)
+                btype = ct.get_entity_type(bid)
+                if(btype == EntityType.HARVESTER):
+                    if(ctx.CHECK_ore_linked_core(ct, self.fix_target)):
+                        self.state = "EXPLORE"
+                        return
+                    self.fix_target = self.GET_start_build_pos(ct, self.fix_target)
 
+            x = self.GUARD_link_back_core(ct, self.fix_target)
+            print("LINK BACK CORE:", x)
+            if x in ["DONE", "STUCK"]:
+                self.state = "EXPLORE"
+                return
         # WORK
-        self.GUARD_wandering(ct)
+
 
         self.GUARD_invariant_action(ct)
 
@@ -330,6 +458,7 @@ class BldGuard():
         self.DEBUG_guard_info(ct)
         self.DEBUG_guard_hotspot(ct)
         # ctx.ORE_debug()
+
 
     def BUILDER_heal_lowest_tile(self, ct):
         best_hp_pos = Position(-1, -1)
